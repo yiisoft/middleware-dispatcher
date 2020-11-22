baf022d76abbcf2b3c1efcf1284e1d5ba22936b8
Alexander Makarov <sam@rmcreative.ru>
2020-11-22 16:36:29 +0300

    Add events

--- src/MiddlewareStack.php
+++ src/MiddlewareStack.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Yiisoft\Middleware\Dispatcher;
 
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Yiisoft\Middleware\Dispatcher\Event\AfterMiddleware;
+use Yiisoft\Middleware\Dispatcher\Event\BeforeMiddleware;
 
 final class MiddlewareStack implements MiddlewareStackInterface
 {
@@ -18,6 +21,13 @@ final class MiddlewareStack implements MiddlewareStackInterface
      * @var RequestHandlerInterface|null stack of middleware
      */
     private ?RequestHandlerInterface $stack = null;
+
+    private EventDispatcherInterface $eventDispatcher;
+
+    public function __construct(EventDispatcherInterface $eventDispatcher)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+    }
 
     public function build(array $middlewares, RequestHandlerInterface $fallbackHandler): MiddlewareStackInterface
     {
@@ -56,19 +66,28 @@ final class MiddlewareStack implements MiddlewareStackInterface
      */
     private function wrap(MiddlewareInterface $middleware, RequestHandlerInterface $handler): RequestHandlerInterface
     {
-        return new class($middleware, $handler) implements RequestHandlerInterface {
+        return new class($middleware, $handler, $this->eventDispatcher) implements RequestHandlerInterface {
             private MiddlewareInterface $middleware;
             private RequestHandlerInterface $handler;
+            private EventDispatcherInterface $eventDispatcher;
 
-            public function __construct(MiddlewareInterface $middleware, RequestHandlerInterface $handler)
+            public function __construct(MiddlewareInterface $middleware, RequestHandlerInterface $handler, EventDispatcherInterface $eventDispatcher)
             {
                 $this->middleware = $middleware;
                 $this->handler = $handler;
+                $this->eventDispatcher = $eventDispatcher;
             }
 
             public function handle(ServerRequestInterface $request): ResponseInterface
             {
-                return $this->middleware->process($request, $this->handler);
+                $this->eventDispatcher->dispatch(new BeforeMiddleware($this->middleware, $request));
+
+                $response = null;
+                try {
+                    return $response = $this->middleware->process($request, $this->handler);
+                } finally {
+                    $this->eventDispatcher->dispatch(new AfterMiddleware($this->middleware, $response));
+                }
             }
         };
     }
