050b18977cba0f840945c6c96f0beafecb456aa0
Sergey Predvoditelev <sergey.predvoditelev@gmail.com>
2021-01-03 23:16:33 +0300

    Style.

--- src/MiddlewareFactory.php
+++ src/MiddlewareFactory.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Yiisoft\Middleware\Dispatcher;
 
-use InvalidArgumentException;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
