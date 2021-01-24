0f6211104d93df3b39b43a88fa8211c2eaaa39dd
Alexey Gevondyan <alexgivi@nextmail.ru>
2021-01-24 18:25:06 +0300

    rename fallback handler in docs

--- src/MiddlewareStackInterface.php
+++ src/MiddlewareStackInterface.php
@@ -10,24 +10,24 @@ use Psr\Http\Server\RequestHandlerInterface;
 interface MiddlewareStackInterface extends RequestHandlerInterface
 {
     /**
-     * Builds middleware stack from array of middleware instances and the main request handler.
+     * Builds middleware stack from array of middleware instances and the fallback request handler.
      * It's possible not to pass any middlewares i. e. set `$middlewares` to `[]`,
-     * than only the main handler will be used.
+     * than only the fallback handler will be used.
      *
      * @param MiddlewareInterface[] $middlewares Middlewares being composed to stack. Can be empty.
-     * @param RequestHandlerInterface $fallbackHandler Main request handler.
+     * @param RequestHandlerInterface $fallbackHandler Fallback request handler.
      *
      * @return self
      */
     public function build(array $middlewares, RequestHandlerInterface $fallbackHandler): self;
 
     /**
-     * Clears the middleware stack including main request handler.
+     * Clears the middleware stack including fallback request handler.
      */
     public function reset(): void;
 
     /**
-     * Checks if there are any middlewares or handler bound.
+     * Checks if there are any middlewares or fallback request handler bound.
      *
      * @return bool
      */
