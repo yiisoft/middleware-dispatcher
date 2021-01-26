3f33c8bdc3e661a919b40b8fe8b759d8c50f30a9
Алексей <alexgivi@nextmail.ru>
2021-01-26 16:43:42 +0300

    Add MiddlewareStackInteface phpDoc
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- src/MiddlewareStackInterface.php
+++ src/MiddlewareStackInterface.php
@@ -4,13 +4,33 @@ declare(strict_types=1);
 
 namespace Yiisoft\Middleware\Dispatcher;
 
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
+/**
+ * A stack of PSR-15 middlewares to be used with {@see MiddlewareDispatcher}.
+ * @see https://www.php-fig.org/psr/psr-15/
+ */
 interface MiddlewareStackInterface extends RequestHandlerInterface
 {
+    /**
+     * Builds a middleware stack from an array of middleware instances and a fallback request handler.
+     * If the middlewares array is empty, only the fallback handler will be used.
+     *
+     * @param MiddlewareInterface[] $middlewares Middlewares being composed to stack. Can be empty.
+     * @param RequestHandlerInterface $fallbackHandler Fallback request handler.
+     *
+     * @return self
+     */
     public function build(array $middlewares, RequestHandlerInterface $fallbackHandler): self;
 
+    /**
+     * Clears the middleware stack and fallback request handler.
+     */
     public function reset(): void;
 
+    /**
+     * @return bool Whether there are any middlewares or fallback request handler bound.
+     */
     public function isEmpty(): bool;
 }
