0c1548f5b836b0954cc81db6d160f638f96a1139
yiiliveext <yiiliveext@gmail.com>
2021-03-12 15:58:54 +0200

    Cleanup

--- tests/MiddlewareDispatcherTest.php
+++ tests/MiddlewareDispatcherTest.php
@@ -18,7 +18,6 @@ use Yiisoft\Middleware\Dispatcher\MiddlewareDispatcher;
 use Yiisoft\Middleware\Dispatcher\MiddlewareFactory;
 use Yiisoft\Middleware\Dispatcher\MiddlewareStack;
 use Yiisoft\Middleware\Dispatcher\Tests\Support\FailMiddleware;
-use Yiisoft\Middleware\Dispatcher\Tests\Support\MockEventDispatcher;
 use Yiisoft\Middleware\Dispatcher\Tests\Support\TestController;
 use Yiisoft\Middleware\Dispatcher\Tests\Support\TestMiddleware;
 use Yiisoft\Test\Support\Container\SimpleContainer;
