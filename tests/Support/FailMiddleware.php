02ea7edf1dd46503934e4bf0c0bacdcb6f91d027
yiiliveext <yiiliveext@gmail.com>
2021-03-12 15:17:52 +0200

    Cleaning

--- tests/Support/FailMiddleware.php
+++ tests/Support/FailMiddleware.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Yiisoft\Middleware\Dispatcher\Tests\Support;
 
-use Nyholm\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
